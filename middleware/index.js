5635fefaca7693d38a7a1ae454f9c6a86674e1f7
Colin Reininger <reiningc@oregonstate.edu>
2018-09-10 15:19:33 -0600

    Add logged in check

---
+++ middleware/index.js
@@ -0,0 +1,13 @@
+var Meal = require("../models/meal");
+var Comment = require("../models/comment");
+
+var middlewareObj = {};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
