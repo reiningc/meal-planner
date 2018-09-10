107365efd81b400d9dc1d79db9143948ac4f9cee
Colin Reininger <reiningc@oregonstate.edu>
2018-09-10 14:57:41 -0600

    Add passport and initial config

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    Meal       = require("./models/meal"),
-    Comment    = require("./models/comment"),
-    seedDB     = require("./seeds");
-
-seedDB();
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Meal            = require("./models/meal"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
 
 var mealRoutes = require("./routes/meals"),
     indexRoutes = require("./routes/index"),
@@ -16,6 +17,19 @@ mongoose.connect("mongodb://localhost:27017/vgmp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+seedDB();
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Jack is a peanut",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use("/", indexRoutes);
 app.use("/meals", mealRoutes);
