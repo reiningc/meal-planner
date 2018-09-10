a820fe44652f54657f49bce5453f271e42b175a4
Colin Reininger <reiningc@oregonstate.edu>
2018-09-10 14:57:09 -0600

    Add registration routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require("express");
+var passport = require("passport");
 var router = express.Router();
+var User = require("../models/user");
 
 
 router.get("/", function(req, res){
     res.render("landing");
 });
+
+// show registration form
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handle signup logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("register");
+        } 
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/meals");
+        });
+    });
+});
+
+
 
 module.exports = router;
