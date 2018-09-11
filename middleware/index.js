782e009b5433071a838bebe8f70893357f97f1c6
Colin Reininger <reiningc@oregonstate.edu>
2018-09-10 22:13:22 -0600

    Add meal ownership check

--- middleware/index.js
+++ middleware/index.js
@@ -10,4 +10,25 @@ middlewareObj.isLoggedIn = function(req, res, next){
     res.redirect("/login");
 }
 
+middlewareObj.checkMealOwnership = function(req, res, next){
+    // user logged in?
+    if(req.isAuthenticated()){
+        Meal.findById(req.params.id, function(err, foundMeal){
+            if(err){
+                res.redirect("back");
+            } else {
+                // user owns meal? check using mongoose .equals() to compare meal.author.id (object type) to req.user._id (string)
+                if(foundMeal.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.redirect("back");
+    }
+}
+
+
 module.exports = middlewareObj;
