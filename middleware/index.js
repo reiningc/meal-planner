016921d69c1b370510317365716a76d2dc0e6529
Colin Reininger <reiningc@oregonstate.edu>
2018-09-10 22:58:50 -0600

    Add comment ownership check

--- middleware/index.js
+++ middleware/index.js
@@ -30,5 +30,23 @@ middlewareObj.checkMealOwnership = function(req, res, next){
     }
 }
 
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else {
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
 
 module.exports = middlewareObj;
