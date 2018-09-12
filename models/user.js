67dacc8d8e8a04271fb88d54ea4c7f4178e539b3
Colin Reininger <reiningc@oregonstate.edu>
2018-09-11 20:57:37 -0600

    Add Plans to User schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    plans: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Plan"
+        }
+    ]
 });
 
 userSchema.plugin(passportLocalMongoose);
